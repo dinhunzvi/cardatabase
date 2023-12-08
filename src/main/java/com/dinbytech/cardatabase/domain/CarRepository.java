ca1a18df6206ecde3abdb97ef3b48f67b4f31637
Douglas Nhunzvi <dougiedj@gmail.com>
2023-12-08 15:17:46 +0200

    Car REST API

--- src/main/java/com/dinbytech/cardatabase/domain/CarRepository.java
+++ src/main/java/com/dinbytech/cardatabase/domain/CarRepository.java
@@ -1,23 +1,17 @@
 package com.dinbytech.cardatabase.domain;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
-    // fetch cars by brand and model
-    List<Car> findByBrandAndModel(String brand, String model);
-    // fetch cars by brand or color
-    List<Car> findByBrandOrColor(String brand, String color);
-    // fetch cars by brand and sort by year
-    List<Car> findByBrandOrderByYearmanufactureAsc(String brand);
-    // fetch cars by brand using SQL
-    @Query("select c from Car c where c.brand = ?1")
-    List<Car> findByBrand(String brand);
-    // fetch cars by brand using SQL
-    @Query("select c from Car c where c.brand like %?1")
-    List<Car> findByBrandEndsWith(String brand);
+    // fetch cars by brand
+    List<Car> findByBrand(@Param("brand") String brand);
+
+    // fetch cars by color
+    List<Car> findByColor(@Param("color") String color);
 
 }
